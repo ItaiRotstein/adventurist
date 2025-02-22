7063495b8d9fa90d03a3edd1b6b759020af97362
Itai Rotstein <itairotstein@gmail.com>
2025-02-22 16:55:12 +0300

    Social Icon and Favourite Icon  - added tooltip

--- frontend/src/cmps/SpotDetails.jsx
+++ frontend/src/cmps/SpotDetails.jsx
@@ -30,10 +30,20 @@ export function SpotDetails({ spot }) {
         <div className="mt-4">
             <h1 className="main-layout text-2xl py-4 font-bold text-center">{spot?.name}</h1>
             <div className="main-layout flex gap-2 justify-end items-center mb-4">
-                <ShareButtons />
-                <button className="shadow p-2 rounded-full hover:brightness-75" onClick={() => toggleFavorite(spot._id)}>
-                    <Icon icon="FaHeart" className={`text-[24px] ${isFavorite ? 'text-clr3' : 'text-gray-300'}`} />
-                </button>
+                <div className="relative group">
+                    <ShareButtons />
+                    <div className="absolute -left-7 top-[50px] mb-2 opacity-0 group-hover:opacity-100 items-center justify-center bg-gray-800 text-white text-sm px-3 py-2 rounded-md whitespace-nowrap before:content-['▲'] before:text-gray-800 before:absolute before:bottom-7 before:left-[40px]">
+                        שתף ברשתות
+                    </div>
+                </div>
+                <div className="relative group">
+                    <button className="shadow p-2 rounded-full hover:brightness-75" onClick={() => toggleFavorite(spot._id)}>
+                        <Icon icon="FaHeart" className={`text-[24px] ${isFavorite ? 'text-clr3' : 'text-gray-300'}`} />
+                    </button>
+                    <div className="absolute left-0 top-[50px] mb-2 hidden group-hover:flex items-center justify-center bg-gray-800 text-white text-sm px-3 py-2 rounded-md whitespace-nowrap before:content-['▲'] before:text-gray-800 before:absolute before:bottom-7 before:left-3">
+                        {isFavorite ? 'הסר ממועדפים' : 'הוסף למועדפים'}
+                    </div>
+                </div>
             </div>
             <div className="w-full h-60">
                 <img src={spot?.img} alt={spot?.name} className="w-full h-full object-cover rounded" />
@@ -73,7 +83,7 @@ export function SpotDetails({ spot }) {
                 {isShowMore ? (
                     <>
                         {moreDetailsKeys.map(({ key, label }) => (
-                            <li key={key} className="border-b py-3">
+                            <li key={key} className="border-b py-3 flex items-center gap-2">
                                 <strong>{label}:</strong>
                                 {spot?.[key] ? (
                                     <span className="inline text-green-600 text-2xl"> ✔︎ </span>
